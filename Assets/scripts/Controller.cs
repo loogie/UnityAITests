a4efca503110255f24ab17e2df33e3e0f626c9fb
Greg Spiridonov <gspiridonov@gmail.com>
2017-08-10 17:00:32 -0400

    Initial Commit

---
+++ Assets/scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public List<GameObject> Areas;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		
+	}
+}
